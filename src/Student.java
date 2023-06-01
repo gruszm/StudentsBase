e1017c35a65abc8dab1320be48287a6e00975146
gruszm <mariuszgruszczynski11@gmail.com>
2023-06-01 21:55:11 +0200

    Students Base - first try

---
+++ src/Student.java
@@ -0,0 +1,26 @@
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String indexNumber;
+
+    @OneToMany(mappedBy = "student", cascade = CascadeType.ALL)
+    private List<Grade> grades;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+    public String getIndexNumber() { return indexNumber; }
+    public void setIndexNumber(String indexNumber) { this.indexNumber = indexNumber; }
+    public List<Grade> getGrades() { return grades; }
+    public void setGrades(List<Grade> grades) { this.grades = grades; }
+}
---
+++ src/SubjectRepository.java
@@ -0,0 +1,3 @@
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubjectRepository extends JpaRepository<Subject, Long> {}
