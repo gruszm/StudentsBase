703de1ce8694f98f4b558b73b463e99d77d39519
gruszm <mariuszgruszczynski11@gmail.com>
2023-06-08 19:54:38 +0200

    Spring Boot initialization fix

--- src/GradeRepository.java
+++
@@ -1,3 +0,0 @@
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GradeRepository extends JpaRepository<Grade, Long> {}
--- src/SubjectRepository.java
+++
@@ -1,3 +0,0 @@
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubjectRepository extends JpaRepository<Subject, Long> {}
