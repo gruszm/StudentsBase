f3e4975720d2cbac1b627e8940c81cb0e70be602
maresyp <mattq.2000@icloud.com>
2023-06-14 18:54:39 +0200

    update

--- src/main/java/com/StudentsBase/StudentsBase/StudentController.java
+++ src/main/java/com/StudentsBase/StudentsBase/StudentController.java
@@ -1,11 +1,13 @@
 package com.StudentsBase.StudentsBase;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/students")
 public class StudentController {
     private final StudentService studentService;
@@ -15,45 +17,68 @@ public class StudentController {
         this.studentService = studentService;
     }
 
-    @GetMapping
     public List<Student> getStudents() {
         return studentService.getStudents();
     }
 
-    @GetMapping("/{id}")
-    public Student getStudent(@PathVariable Long id) {
-        return studentService.getStudent(id);
+    @GetMapping("/")
+    public String home_page(Model model) {
+        model.addAttribute("students", getStudents());
+        return "home";
     }
 
-    @PostMapping
-    public Student addStudent(@RequestBody Student student) {
-        return studentService.addStudent(student);
+    @GetMapping("/add")
+    public String add_student(Model model) {
+        return "add_student";
     }
 
-    @DeleteMapping("/{id}")
-    public void deleteStudent(@PathVariable Long id) {
+    @PostMapping(value = "/addStudent", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public String addStudent(@ModelAttribute Student student) {
+        System.out.println(student);
+        studentService.addStudent(student);
+        return "redirect:/students/";
+    }
+
+     @GetMapping("/edit/{id}")
+     public String getStudent(@PathVariable Long id, Model model) {
+        Student student = studentService.getStudent(id); // TODO : add error handling
+        model.addAttribute("student", student);
+        return "edit_student";
+    }
+
+     @DeleteMapping("/delete/{id}")
+     public String deleteStudent(@PathVariable("id") Long id) {
+        System.out.println("Deleting student with id: " + id);
         studentService.deleteStudent(id);
-    }
+        return "redirect:/students/";
+     }
 
-    @PutMapping("/{id}")
-    public Student updateStudent(@PathVariable Long id, @RequestBody Student student) {
-        return studentService.updateStudent(id, student);
-    }
+     @PutMapping("/edit/{id}")
+     public String updateStudent(@PathVariable Long id, @ModelAttribute Student
+     student, Model model) {
+        System.out.println("Updating student with id: " + id);
+        Student updated_student = studentService.updateStudent(id, student);
+        model.addAttribute("student", updated_student);
+        return "edit_student";
+     }
 
-    @GetMapping("/{studentId}/subjects")
-    public List<Subject> getSubjectsAssignedToStudent(@PathVariable Long studentId)
-    {
-        return studentService.getSubjectsAssignedToStudent(studentId);
-    }
-
-    @PostMapping("/{studentId}/subjects/{subjectId}")
-    public Student addSubjectToStudent(@PathVariable Long studentId, @PathVariable Long subjectId) {
-        return studentService.addSubjectToStudent(studentId, subjectId);
-    }
-
-    @GetMapping("{studentId}/grades")
-    public List<Grade> getGrades(@PathVariable Long studentId)
-    {
-        return studentService.getGrades(studentId);
-    }
+    //
+    // @GetMapping("/{studentId}/subjects")
+    // public List<Subject> getSubjectsAssignedToStudent(@PathVariable Long
+    // studentId)
+    // {
+    // return studentService.getSubjectsAssignedToStudent(studentId);
+    // }
+    //
+    // @PostMapping("/{studentId}/subjects/{subjectId}")
+    // public Student addSubjectToStudent(@PathVariable Long studentId,
+    // @PathVariable Long subjectId) {
+    // return studentService.addSubjectToStudent(studentId, subjectId);
+    // }
+    //
+    // @GetMapping("{studentId}/grades")
+    // public List<Grade> getGrades(@PathVariable Long studentId)
+    // {
+    // return studentService.getGrades(studentId);
+    // }
 }
--- src/main/java/com/StudentsBase/StudentsBase/StudentsBaseApplication.java
+++ src/main/java/com/StudentsBase/StudentsBase/StudentsBaseApplication.java
@@ -2,7 +2,6 @@ package com.StudentsBase.StudentsBase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -13,3 +12,4 @@ public class StudentsBaseApplication {
 		SpringApplication.run(StudentsBaseApplication.class, args);
 	}
 }
+
