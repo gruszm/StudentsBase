9650533fdd624956e8677108e5ad5685209e6ce5
maresyp <mattq.2000@icloud.com>
2023-06-17 14:30:53 +0200

    fix missing implementation

--- src/main/java/com/StudentsBase/StudentsBase/StudentController.java
+++ src/main/java/com/StudentsBase/StudentsBase/StudentController.java
@@ -92,7 +92,17 @@ public class StudentController
      */
     @GetMapping("/edit/{id}")
     public String getStudent(@PathVariable Long id, Model model) {
-        // method implementation
+        Student student = studentService.getStudent(id);
+        List<Subject> availableSubjects = subjectService.getSubjects().stream()
+                .filter(subject -> !student.getSubjects().contains(subject))
+                .collect(Collectors.toList());
+        List<Grade> grades = studentService.getGrades(id);
+        List<Double> possibleGrades = Arrays.asList(2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0);
+
+        model.addAttribute("possibleGrades", possibleGrades);
+        model.addAttribute("student", student);
+        model.addAttribute("subjects", availableSubjects);
+        model.addAttribute("grades", grades);
         return "edit_student";
     }
 
@@ -117,7 +127,7 @@ public class StudentController
      * @return a string indicating the view name
      */
     @PutMapping("/edit/{id}")
-    public String updateStudent(@PathVariable Long id, @ModelAttribute Student student, Model model) {
+     public String updateStudent(@PathVariable Long id, @ModelAttribute Student student, Model model) {
         studentService.updateStudent(id, student);
         return getStudent(id, model);
     }
@@ -138,7 +148,14 @@ public class StudentController
                             @RequestParam("studentId") Long studentId,
                             @RequestParam("subjectId") Long subjectId,
                             Model model) {
-        // method implementation
+        Grade grade = studentService.getGrade(studentId, subjectId);
+        if (mark == null) {
+            grade.setMark(null);
+        } else {
+            grade.setMark(mark);
+        }
+        gradeService.addGrade(grade);
+
         return getStudent(studentId, model);
     }
 
@@ -151,7 +168,11 @@ public class StudentController
      */
     @GetMapping("/inspect/{id}")
     public String inspectStudent(@PathVariable Long id, Model model) {
-        // method implementation
+        Student student = studentService.getStudent(id);
+        List<Grade> grades = studentService.getGrades(id);
+
+        model.addAttribute("student", student);
+        model.addAttribute("grades", grades);
         return "inspect_student";
     }
 
